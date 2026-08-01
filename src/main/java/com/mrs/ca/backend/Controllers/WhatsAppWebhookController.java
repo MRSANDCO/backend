614078c9b270e1a94666154a42865f118cb31332
deepma <jaiswaldeepma1283@gmail.com>
2026-08-01 22:52:09 +0530

    webhook updated again

--- src/main/java/com/mrs/ca/backend/Controllers/WhatsAppWebhookController.java
+++ src/main/java/com/mrs/ca/backend/Controllers/WhatsAppWebhookController.java
@@ -1,4 +1,13 @@
 // Webhook controller for Twilio removed.
+package com.mrs.ca.backend.Controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/api/whatsapp")
 public class WhatsAppWebhookController {
